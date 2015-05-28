9ef3b697bac4e752cc8a8272222782c737fb4da2
BrunoRissi <rissi@gir.srv.br>
2015-05-28 14:58:44 -0300

    Tipo compromisso parte  3

--- GIR.Sigim/GIR.Sigim.Infrastructure.Data/Repository/Financeiro/TipoDocumentoRepository.cs
+++ GIR.Sigim/GIR.Sigim.Infrastructure.Data/Repository/Financeiro/TipoDocumentoRepository.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using GIR.Sigim.Domain.Entity.Financeiro;
 using GIR.Sigim.Domain.Repository.Financeiro;
+using GIR.Sigim.Domain.Specification;
 
 namespace GIR.Sigim.Infrastructure.Data.Repository.Financeiro
 {
@@ -18,5 +21,45 @@ namespace GIR.Sigim.Infrastructure.Data.Repository.Financeiro
 
         }
         #endregion
+
+        #region IRepository<TEntity> Members
+
+        public override IEnumerable<TipoDocumento> ListarPeloFiltroComPaginacao(
+            ISpecification<TipoDocumento> specification,
+            int pageIndex,
+            int pageCount,
+            string orderBy,
+            bool ascending,
+            out int totalRecords,
+            params Expression<Func<TipoDocumento, object>>[] includes)
+        {
+            var set = CreateSetAsQueryable(includes);
+
+            set = set.Where(specification.SatisfiedBy());
+
+            totalRecords = set.Count();
+
+            switch (orderBy)
+            {
+                case "id":
+                    set = ascending ? set.OrderBy(l => l.Id) : set.OrderByDescending(l => l.Id);
+                    break;
+                case "sigla":
+                    set = ascending ? set.OrderBy(l => l.Sigla) : set.OrderByDescending(l => l.Sigla);
+                    break;
+                case "descricao":
+                    set = ascending ? set.OrderBy(l => l.Descricao) : set.OrderByDescending(l => l.Descricao);
+                    break;
+                case "codigo":
+                default:
+                    set = ascending ? set.OrderBy(l => l.Id) : set.OrderByDescending(l => l.Id);
+                    break;
+            }
+
+            return set.Skip(pageCount * pageIndex).Take(pageCount);
+        }
+
+        #endregion
+
     }
 }
