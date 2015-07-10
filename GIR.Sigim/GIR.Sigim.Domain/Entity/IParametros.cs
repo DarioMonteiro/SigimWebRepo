1b5474cfe4d6ea6dff72adee61c818c1ace44afe
wilsonmarques <wilson.marques@gmail.com>
2015-07-10 01:42:49 -0300

    Ajuste para obter ícone e nome da empresa do centro de custo na impressão da requisição de material.

---
+++ GIR.Sigim/GIR.Sigim.Domain/Entity/IParametros.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GIR.Sigim.Domain.Entity.Sigim;
+
+namespace GIR.Sigim.Domain.Entity
+{
+    public interface IParametros
+    {
+        byte[] IconeRelatorio { get; set; }
+        ClienteFornecedor Cliente { get; set; }
+    }
+}
