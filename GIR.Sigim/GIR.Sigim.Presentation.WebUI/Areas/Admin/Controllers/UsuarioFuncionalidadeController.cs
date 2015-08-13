65ab1f0aa80475184e95e86dff0b52aebb7d0a83
DarioMonteiro <dario@gir.srv.br>
2015-08-13 16:36:01 -0300

    Correção na página usuário por funcionalidade , na combo de usuário , para ordenar os usuário por ordem alfabética de login . Módulo Admin;

--- GIR.Sigim/GIR.Sigim.Presentation.WebUI/Areas/Admin/Controllers/UsuarioFuncionalidadeController.cs
+++ GIR.Sigim/GIR.Sigim.Presentation.WebUI/Areas/Admin/Controllers/UsuarioFuncionalidadeController.cs
@@ -238,7 +238,7 @@ namespace GIR.Sigim.Presentation.WebUI.Areas.Admin.Controllers
 
         private void CarregarCombos(UsuarioFuncionalidadeViewModel model)
         {
-            model.ListaUsuario = new SelectList(usuarioAppService.ListarTodos(), "Id", "Login", model.UsuarioId);
+            model.ListaUsuario = new SelectList(usuarioAppService.ListarTodos().OrderBy(l => l.Login), "Id", "Login", model.UsuarioId);
             model.ListaModulo = new SelectList(moduloAppService.ListarTodos(), "Id", "NomeCompleto", model.ModuloId);
             model.ListaPerfil = new SelectList(perfilAppService.ListarPeloModulo(model.ModuloId), "Id", "Descricao", model.PerfilId);
         }
