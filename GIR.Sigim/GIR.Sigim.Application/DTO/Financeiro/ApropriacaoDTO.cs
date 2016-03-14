9c1f5c8ccbefcc89126168dd9b48ee24e2b13c08
BrunoRissi <rissi@gir.srv.br>
2016-03-14 07:19:42 -0300

    Inicio Rel. Titulo a pagar

---
+++ GIR.Sigim/GIR.Sigim.Application/DTO/Financeiro/ApropriacaoDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GIR.Sigim.Application.DTO.Contrato;
+using GIR.Sigim.Application.DTO.Financeiro;
+
+namespace GIR.Sigim.Application.DTO.Financeiro
+{
+    public class ApropriacaoDTO : BaseDTO
+    {
+        public string CodigoClasse { get; set; }
+        public ClasseDTO Classe { get; set; }
+        public string CodigoCentroCusto { get; set; }
+        public CentroCustoDTO CentroCusto { get; set; }
+        public int? TituloPagarId { get; set; }
+        public TituloPagarDTO TituloPagar { get; set; }
+        public int? TituloReceberId { get; set; }
+        public TituloReceberDTO TituloReceber { get; set; }
+        public int? MovimentoId { get; set; }
+        public MovimentoFinanceiroDTO Movimento { get; set; }
+        public decimal Valor { get; set; }
+        public decimal Percentual { get; set; }
+    }
+}
--- GIR.Sigim/GIR.Sigim.Application/DTO/Financeiro/TituloPagarDTO.cs
+++ GIR.Sigim/GIR.Sigim.Application/DTO/Financeiro/TituloPagarDTO.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using GIR.Sigim.Application.DTO.Contrato;
+using GIR.Sigim.Application.DTO.Financeiro;
 
 namespace GIR.Sigim.Application.DTO.Financeiro
 {
@@ -11,11 +12,13 @@ namespace GIR.Sigim.Application.DTO.Financeiro
     {
         public List<TituloPagarDTO> ListaFilhos { get; set; }
         public List<ImpostoPagarDTO> ListaImpostoPagar { get; set; }
+        public List<ApropriacaoDTO> ListaApropriacao { get; set; }
 
         public TituloPagarDTO()
         {
             this.ListaFilhos = new List<TituloPagarDTO>();
             this.ListaImpostoPagar = new List<ImpostoPagarDTO>();
+            this.ListaApropriacao = new List<ApropriacaoDTO>();
         }
     }
 }
