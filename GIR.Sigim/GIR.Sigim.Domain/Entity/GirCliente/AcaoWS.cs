0498e04d501a9bdb4dd50c6f9bceb576bc3463ac
DarioMonteiro <dario@gir.srv.br>
2016-04-05 16:27:24 -0300

    Implementação controle de acesso ao módulo.

---
+++ GIR.Sigim/GIR.Sigim.Domain/Entity/GirCliente/AcaoWS.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel; 
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIR.Sigim.Domain.Entity.GirCliente
+{
+    public enum AcaoWS
+    {
+        [Description("Não Acessou")]
+        NaoAcessou = 0,
+
+        [Description("Acessou")]
+        Acessou = 1,
+
+        [Description("AcessouComErro")]
+        AcessouComErro = 2
+    }
+}
